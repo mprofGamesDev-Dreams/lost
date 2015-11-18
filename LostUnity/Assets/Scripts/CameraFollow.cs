8db174bc871f7bac50d6cb1902b5df727cbd7815
unknown <1100484@MC10801.uad.ac.uk>
2015-11-18 15:24:08 +0000

    Added camera tracking. Revised third person controller to work independant from the camera. Added third person assets.

---
+++ LostUnity/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Range(2.0f, 100.0f)]
+    public float FollowDistance;
+
+    public GameObject PlayerPrefab;
+
+	void Start ()
+    {
+        if (!PlayerPrefab)
+        {
+            Debug.Log("No Player Attached to Camera");
+        }
+	}
+	
+	void Update ()
+    {
+        // Retrieve the position of the player
+        Vector3 trackingPosition = PlayerPrefab.transform.position;
+
+        // Adjust the tracking position
+        trackingPosition.y += FollowDistance;
+
+        // Apply the position to the object
+        this.gameObject.transform.position = trackingPosition;
+	}
+}
---
+++ LostUnity/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class ThirdPersonUserControl : MonoBehaviour
+    {
+        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+        private Vector3 m_CamForward;             // The current forward direction of the camera
+        private Vector3 m_Move;
+        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+        
+        private void Start()
+        {
+            // get the third person character ( this should never be null due to require component )
+            m_Character = GetComponent<ThirdPersonCharacter>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+            // read inputs
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            bool crouch = Input.GetKey(KeyCode.C);
+
+            // calculate move direction to pass to character
+            m_Move = v*transform.forward + h*transform.right;
+#if !MOBILE_INPUT
+			// walk speed multiplier
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+#endif
+
+            // pass all parameters to the character control script
+            m_Character.Move(m_Move, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
