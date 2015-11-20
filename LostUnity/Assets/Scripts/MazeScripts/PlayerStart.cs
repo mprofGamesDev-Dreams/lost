b22b3f63659b5e74f0e920c55b9aa169b7b410c5
unknown <1405362@MC10791.uad.ac.uk>
2015-11-20 13:31:15 +0000

    new lighting commit

---
+++ LostUnity/Assets/Scripts/MazeScripts/PlayerStart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStart : MonoBehaviour {
+
+	private GameObject mazeGameObject;
+
+	// Use this for initialization
+	void Start () {
+		mazeGameObject = GameObject.Find ("Maze");
+		MazeObject mazeComponent = mazeGameObject.GetComponent<MazeObject> ();
+
+		//Setup player position
+		gameObject.transform.position = new Vector3
+			(mazeGameObject.transform.position.x + (mazeComponent.TileWidth * mazeComponent.StartX),
+			mazeGameObject.transform.position.y + 1.0f,
+			mazeGameObject.transform.position.z + (mazeComponent.TileHeight * mazeComponent.StartY));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
